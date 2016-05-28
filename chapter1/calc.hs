96f4b6021da087b11721b71a2a6fa0969aa9eb77
Alex Koloskov <alexandr.koloskov@gmail.com>
2016-05-27 17:07:28 -0700

    Moved chapter 1 exercises into a separate folder

---
+++ chapter1/calc.hs
@@ -0,0 +1,6 @@
+module Main where
+import System.Environment
+
+main :: IO ()
+main = do args <- getArgs
+          putStrLn (show(read(args!!0) + read(args!!1)))
---
+++ chapter1/hello-readline.hs
@@ -0,0 +1,6 @@
+module Main where
+import System.Environment
+
+main :: IO ()
+main = do name <- getLine
+          putStrLn ("Hello " ++ name)
